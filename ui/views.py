f429d201aeb7e5ef41e136e128c8639c9c28e092
ryanho <204075+ryanho@users.noreply.github.com>
2024-03-23 12:04:44 +0800

    變更格式

--- ui/views.py
+++ ui/views.py
@@ -147,7 +147,7 @@ def webhook(request):
                 mention_user = re.findall(r'@\w+', content)
                 if mention_user:
                     for at_user in mention_user:
-                        content = content.replace(at_user, f"<a href=\"{result['server']}/{at_user}\">{at_user}</a>")
+                        content = content.replace(at_user, f"{result['server']}/{at_user}")
 
                 raw_emojis = re.findall(r':\w+:', content)
                 if raw_emojis:
